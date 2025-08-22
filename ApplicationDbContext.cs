3553962113b4209931faf35a2c0794fbe7c2bbf4
Alex McCullough <alex.mccullough@kainos.com>
2025-08-22 13:44:08 +0100

    fix: enums properly handled

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,8 +1,22 @@
+using System.Globalization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var bandConvertor = new ValueConverter<Band, string>(
+            v => v.ToString().ToLower(),
+            v => Enum.Parse<Band>(CultureInfo.InvariantCulture.TextInfo.ToTitleCase(v))
+        );
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Band)
+            .HasConversion(bandConvertor);
+    }
 }
